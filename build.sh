92a1560ff57045f0d5913ed8cf2489aa6ba281d6
tan0407 <lebreguy@gmail.com>
2018-12-02 19:14:34 +0100

    Modified the build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
-make && make clean && echo "Build successful"
+g++ -o algorithms.o -c ./src/algorithms.cc -Wall -Wno-sign-compare &&
+g++ -o cnfReader.o -c ./src/cnfReader.cc -Wall -Wno-sign-compare &&
+g++ -o solverSAT.o -c ./src/solverSAT.cc -Wall -Wno-sign-compare &&
+g++ -o solverSAT algorithms.o cnfReader.o solverSAT.o &&
+rm -rf *.o &&
+echo "Build successful"
